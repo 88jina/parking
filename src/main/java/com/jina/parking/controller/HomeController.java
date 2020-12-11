9fa7b0c8295b93dda2325c814fd8577de507fba6
88jina <jina940323@gmail.com>
2020-12-11 18:28:51 +0900

    made membership vo

---
+++ src/main/java/com/jina/parking/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.jina.parking.controller;
+
+public class HomeController {
+
+}
---
+++ src/main/java/com/jina/parking/model/Membership.java
@@ -0,0 +1,25 @@
+package com.jina.parking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String carNum;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String phone;
+}
