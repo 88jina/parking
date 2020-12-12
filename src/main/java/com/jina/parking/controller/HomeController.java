23dc7993dbc1c5ec1c4f24e4aa5eabe2ef939fe3
88jina <jina940323@gmail.com>
2020-12-12 22:54:43 +0900

    test

--- src/main/java/com/jina/parking/controller/HomeController.java
+++ src/main/java/com/jina/parking/controller/HomeController.java
@@ -1,5 +1,14 @@
 package com.jina.parking.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name", "Jina");
+        return "hello";
+    }
 }
---
+++ src/test/java/com/jina/parking/test/HomeControllerTest.java
@@ -0,0 +1,29 @@
+package com.jina.parking.test;
+
+import com.jina.parking.controller.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    MockMvc mock;
+
+    @Test
+    public void hello() throws Exception{
+        mock.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name",("Jina")));
+
+    }
+}
